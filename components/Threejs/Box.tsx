c650066847e3bbfb6f093b2e10604da752cd680c
Camillemtd <camille_mtd@hotmail.com>
2024-03-10 03:47:36 -0400

    box

--- components/Threejs/Box.tsx
+++ components/Threejs/Box.tsx
@@ -1,49 +1,147 @@
-// React 
-import { useState, useRef } from "react";
+// React
+import { useState, useRef, useEffect } from "react";
+import gsap from "gsap";
 
 // Three.js
 import { Mesh } from "three";
-import { SpotLight } from "@react-three/drei";
+import { useTexture } from "@react-three/drei";
 import { DoubleSide } from "three";
-
+import * as THREE from "three";
+import { Vector3 } from "@react-three/fiber";
 
 export default function Box({ box }: { box: string }) {
-	const [lightVisible, setLightVisible] = useState(false);
-	const boxRef = useRef();
-  
-	// Gère l'apparition de la lumière lorsque la souris passe sur la boîte
-	const handlePointerOver = () => {
-	  setLightVisible(true);
-	  document.body.style.cursor = 'pointer';
-	};
-  
-	// Gère la disparition de la lumière lorsque la souris quitte la boîte
-	const handlePointerOut = () => {
-	  setLightVisible(false);
-	  document.body.style.cursor = 'default'
-	};
-  
-	const boxPosition: [number, number, number] = box === 'left' ? [-2, -0.5, -1] : [2, -0.5, -1];
-	
+  // State
+  const [isTrapOpen, setIsTrapOpen] = useState(false);
+
+  // Ref
+  const trapGroupRef = useRef<Mesh>(null);
+  const boxRef = useRef();
+  const groupRef = useRef<THREE.Group>(null);
+
+  // Texture
+  const [
+    colorMap,
+    aoMap,
+    emissiveMap,
+    heightMap,
+    metalnessMap,
+    normalMap,
+    roughnessMap,
+  ] = useTexture([
+    "./Texture/box_basecolor.jpg",
+    "./Texture/box_ambientOcclusion.jpg",
+    "./Texture/box_emissive.jpg",
+    "./Texture/box_height.png",
+    "./Texture/box_metallic.jpg",
+    "./Texture/box_normal.jpg",
+    "./Texture/box_roughness.jpg",
+  ]);
+
+  const boxPosition: Vector3 = box === "left" ? [-2, -0.5, 0] : [2, -0.5, 0];
+
+  const handlePointerOver = () => {
+    document.body.style.cursor = "pointer";
+  };
+
+  const handlePointerOut = () => {
+    document.body.style.cursor = "default";
+  };
+
+  const toggleTrap = () => {
+    setIsTrapOpen(!isTrapOpen);
+
+    const opening = !isTrapOpen;
+
+    if (trapGroupRef.current) {
+      const targetRotationX = opening ? Math.PI / 2 : 0;
+
+      const ease = opening ? "elastic.out(1, 0.3)" : "power2.out";
+
+      gsap.to(trapGroupRef.current.rotation, {
+        x: targetRotationX,
+        duration: opening ? 2 : 1,
+        ease: ease,
+      });
+    }
+  };
+
+  const onHover = () => {
+    if (groupRef.current) {
+      gsap.killTweensOf(groupRef.current.position);
+
+      gsap.to(groupRef.current.position, {
+        y: "+=0.3",
+        duration: 1,
+        yoyo: true,
+        repeat: -1,
+        ease: "sine.inOut",
+      });
+    }
+  };
+
+  const onUnhover = () => {
+    if (groupRef.current) {
+      gsap.killTweensOf(groupRef.current.position);
+
+      gsap.to(groupRef.current.position, {
+        y: boxPosition[1],
+        duration: 0.5,
+        ease: "sine.inOut",
+      });
+    }
+  };
+
   return (
-    <group position={boxPosition}>
-      <SpotLight
-	  	position-y={5}
-        distance={5}
-        angle={0.15}
-        attenuation={5}
-        anglePower={5} 
-		visible={lightVisible}
-		target={boxRef.current}
-      />
-      <mesh onPointerOver={handlePointerOver} onPointerOut={handlePointerOut} ref={boxRef}>
-        <boxGeometry args={[1.3, 1.3, 1.3]}/>
-        <meshStandardMaterial color="white" />
+    <group
+      ref={groupRef}
+      position={boxPosition}
+      onPointerOver={onHover}
+      onPointerOut={onUnhover}
+    >
+      <mesh
+        onPointerOver={handlePointerOver}
+        onPointerOut={handlePointerOut}
+        ref={boxRef}
+        onClick={toggleTrap}
+      >
+        <boxGeometry args={[1.3, 1.3, 1.3, 10, 10]} />
+        <meshStandardMaterial
+          map={colorMap}
+          emissive={new THREE.Color("white")}
+          aoMap={aoMap}
+          aoMapIntensity={1}
+          emissiveMap={emissiveMap}
+          emissiveIntensity={1}
+          metalness={1}
+          roughness={0}
+          displacementMap={heightMap}
+          displacementScale={0}
+          metalnessMap={metalnessMap}
+          roughnessMap={roughnessMap}
+          normalMap={normalMap}
+        />
       </mesh>
-	  <mesh position={[0, -0.652, 0]} rotation={[Math.PI * 0.5, 0, 0]}>
-		<planeGeometry args={[1.3, 1.3]}/>
-		<meshStandardMaterial color="white" side={DoubleSide} />
-	  </mesh>
+      <group ref={trapGroupRef} position={[0, -0.655, -0.65]}>
+        <mesh position={[0, 0, 0.65]} rotation={[Math.PI * 0.5, 0, 0]}>
+          <planeGeometry args={[1.3, 1.3, 100, 100]} />
+          <meshStandardMaterial
+            side={DoubleSide}
+            map={colorMap}
+            emissive={new THREE.Color("white")}
+            aoMap={aoMap}
+            aoMapIntensity={1}
+            emissiveMap={emissiveMap}
+            emissiveIntensity={1}
+            metalness={1}
+            roughness={0}
+            displacementMap={heightMap}
+            displacementScale={0}
+            metalnessMap={metalnessMap}
+            roughnessMap={roughnessMap}
+            normalMap={normalMap}
+          />
+        </mesh>
+      </group>
     </group>
   );
 }
--- components/Threejs/Experience.tsx
+++ components/Threejs/Experience.tsx
@@ -1,18 +1,28 @@
 'use client';
 
-import { OrbitControls, Environment } from "@react-three/drei";
+import { OrbitControls, Environment, SpotLight, PerspectiveCamera } from "@react-three/drei";
 import Box from "./Box";
 export default function Experience() {
 	  return (
 	<>
-		<OrbitControls/>
+		<PerspectiveCamera makeDefault position={[0, 0.8, 7.4]} />
+		<OrbitControls />
 		<directionalLight
                 color="#faedcd"
                 intensity={3}
                 position={[3, 2, 0.5]}
                 castShadow
             />
-		<ambientLight intensity={0.5} />
+		<ambientLight intensity={3} />
+		
+		<SpotLight
+		position={[0, 5, 0]}
+		intensity={7}
+        distance={10}
+        angle={1}
+        attenuation={15}
+        anglePower={10}
+      />
 		<Environment background files={"./background2.hdr"} />
 		<Box box={'left'}/>
 		<Box box={'right'}/>
